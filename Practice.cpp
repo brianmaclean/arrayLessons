6d6ba07f636a51028ebbc850f02539ff8ff1fa14
Brian MacLean <brian.maclean@verizon.net>
2018-11-23 20:06:36 -0800

    message

--- Practice.cpp
+++ Practice.cpp
@@ -1,16 +1,19 @@
+//THis function uses a reference variable as a parameter
 #include <iostream>
 using namespace std;
 
-void showStatic();
+void doubleNum (int &refVar);
 
 int main(){
-	for ( int count = 0; count < 5; count++ ){
-		showStatic();
-	}
+	int value = 4;
+
+	cout << "In main, value is " << value << endl;
+	cout << "Now calling doubleNum... " << endl;
+	doubleNum(value);
+	cout << "Now back in main, value is " << value << endl;
 	return 0;
 }
-void showStatic(){
-	static int numCalls = 0;
 
-	cout << "This function has been called " << ++numCalls << " times. " << endl;
+void doubleNum (int &refVar){
+	refVar *= 2;
 }

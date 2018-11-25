72751980668ab0a8644854c4587955279aa97f10
Brian MacLean <brian.maclean@verizon.net>
2018-11-25 10:23:15 -0800

    Array initialization

--- Practice.cpp
+++ Practice.cpp
@@ -1,19 +1,26 @@
-//THis function uses a reference variable as a parameter
 #include <iostream>
+#include <iomanip>
+#include <string>
 using namespace std;
 
-void doubleNum (int &refVar);
-
 int main(){
-	int value = 4;
+	const int NUM_MONTHS = 12;
+	int choice;
+	string name[NUM_MONTHS] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
-	cout << "In main, value is " << value << endl;
-	cout << "Now calling doubleNum... " << endl;
-	doubleNum(value);
-	cout << "Now back in main, value is " << value << endl;
+	int days[NUM_MONTHS] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+	cout << "This program will tell you how many days are in any month. \n\n";
+
+	for ( int month = 1; month <= NUM_MONTHS; month++ ){
+		cout << setw(2) << month << "	" << name[month-1] << endl;
+	}
+
+	cout << "\nEnter the number of the month you want: ";
+	cin >> choice;
+
+	cout << "The month of " << name[choice-1] << " has " << days[choice-1] << " days in it.\n";
+
 	return 0;
-}
-
-void doubleNum (int &refVar){
-	refVar *= 2;
+	
 }
